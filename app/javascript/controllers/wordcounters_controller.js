dfe7e57b9fbf58417e340b457b0483bd6980da02
afeez oloyede <oloyedeafeez@363photography.org>
2022-07-10 18:12:29 -0700

    Fixed the line problem for spell errors

--- app/javascript/controllers/wordcounters_controller.js
+++ app/javascript/controllers/wordcounters_controller.js
@@ -5,6 +5,24 @@ export default class extends Controller {
   static targets = ["item", "form"]
 
   connect() {
+
+    const shortWords = `thiss is intresting intresting as
+    intresting intresting`
+    const newWords = shortWords.replace(/\r\n|\n|\r/g, "")
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '3b739ff8d7msh80f76a14bca1cabp114681jsn833db6b65c4f',
+        'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com'
+      },
+      body: `{"language":"enUS","fieldvalues":"${newWords}","config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}`
+    };
+
+    fetch('https://jspell-checker.p.rapidapi.com/check', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
   }
 
   send (event) {
@@ -64,6 +82,7 @@ export default class extends Controller {
   }
 
   #spellWords (myWord) {
+    const newWord = myWord.replace(/\r\n|\r|\n/g, " ")
     fetch('https://jspell-checker.p.rapidapi.com/check', {
       method: 'POST',
       headers: {
@@ -71,7 +90,7 @@ export default class extends Controller {
         'X-RapidAPI-Key': '3b739ff8d7msh80f76a14bca1cabp114681jsn833db6b65c4f',
         'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com'
       },
-      body: `{"language":"enUS","fieldvalues":"${myWord}","config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}`
+      body: `{"language":"enUS","fieldvalues":"${newWord}","config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}`
     } )
       .then(response => response.json())
       .then(data => this.#forWords(myWord, data))
